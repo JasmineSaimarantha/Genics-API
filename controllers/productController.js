e82baa8c6334e4d27f1132e9c9b28b6e6ad6fe44
unknown <jasminesaimarantha@gmail.com>
2023-07-01 09:59:38 +0700

    add delete and update user

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,102 @@
-const Product = require('../models/Product');
-
+const Product = require("../models/Product");
 
 // Get all products
-const getProducts = async (req, res) => {};
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(201).json({
+      message: "get product success",
+      data: products,
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: "get product fail",
+      data: e,
+    });
+  }
+};
 
 // Save a product
-const saveProduct = async (req, res) => {};
+const saveProduct = async (req, res) => {
+  try {
+    const newProduct = new Product({
+      name: req.body.name,
+      price: req.body.price,
+    });
+    const result = await newProduct.save();
+    res.status(201).json({
+      message: "save product success",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "save product fail",
+      data: error,
+    });
+    console.log(error);
+  }
+};
 
 // Update a product
-const updateProduct = async (req, res) => {};
+const updateProduct = async (req, res) => {
+  try {
+    if (!req?.params?.id) {
+      return res.status(400).json({ message: "must have ID parameter" });
+    }
+    const searchProduct = await Product.findById(req.params.id);
+    if (!searchProduct) {
+      return res
+        .status(204)
+        .json({ message: "no product matches ID ${req.params.id}" });
+    }
+    if (req.body?.name) {
+      searchProduct.name = req.body.name;
+    }
+    if (req.body?.price) {
+      searchProduct.price = req.body.price;
+    }
+    const result = await searchProduct.save();
+    res.status(201).json({
+      message: "update product success",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "update product fail",
+      data: error,
+    });
+    console.log(error);
+  }
+};
 
 // Delete a product
-const deleteProduct = async (req, res) => {};
+const deleteProduct = async (req, res) => {
+  try {
+    if (!req?.params?.id) {
+      return res.status(400).json({ message: "must have ID parameter" });
+    }
+    const searchProduct = await Product.findById(req.params.id);
+    if (!searchProduct) {
+      return res
+        .status(204)
+        .json({ message: "no product matches ID ${req.params.id}" });
+    }
+    const deletes = await searchProduct.deleteOne({_id: req.params.id})
+    res.status(201).json({
+      message: "delete product success",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "delete product fail",
+      data: error,
+    });
+    console.log(error);
+  }
+};
 
 module.exports = {
   getProducts,
   saveProduct,
   updateProduct,
-  deleteProduct
-}
+  deleteProduct,
+};
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,98 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
-const getUsers = async (req, res) => {};
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(201).json({
+      message: "get user success",
+      data: users,
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: "get user fail",
+      data: e,
+    });
+  }
+};
 
-const saveUser = async (req, res) => {};
+const saveUser = async (req, res) => {
+  try {
+    const newUser = new User({
+      name: req.body.name,
+      age: req.body.age,
+    });
+    const result = await newUser.save();
+    res.status(201).json({
+      message: "save user success",
+      data: result,
+    });    
+  } catch (error) {
+    res.status(500).json({
+      message: "save user fail",
+      data: error,
+    });
+    console.log(error);
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    if (!req?.params?.id) {
+      return res.status(400).json({ message: "must have ID parameter" });
+    }
+    const searchUser = await User.findById(req.params.id);
+    if (!searchUser) {
+      return res
+        .status(204)
+        .json({ message: "no product matches ID ${req.params.id}" });
+    }
+    if (req.body?.name) {
+      searchUser.name = req.body.name;
+    }
+    if (req.body?.price) {
+      searchUser.price = req.body.price;
+    }
+    const result = await searchUser.save();
+    res.status(201).json({
+      message: "update user success",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "update user fail",
+      data: error,
+    });
+    console.log(error);
+  }
+}
+
+const deleteUser = async (req, res) => {
+  try {
+    if (!req?.params?.id) {
+      return res.status(400).json({ message: "must have ID parameter" });
+    }
+    const searchUser = await User.findById(req.params.id);
+    if (!searchUser) {
+      return res
+        .status(204)
+        .json({ message: "no product matches ID ${req.params.id}" });
+    }
+    const deletes = await searchUser.deleteOne({_id: req.params.id})
+    res.status(201).json({
+      message: "delete user success",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "delete user fail",
+      data: error,
+    });
+    console.log(error);
+  }
+}
 
 module.exports = {
   getUsers,
-  saveUser
-}
+  saveUser,
+  updateUser,
+  deleteUser
+};
